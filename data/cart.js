2b22e5b915e5d2faf122146377687de260ceafe2
omkar0605shelar <shelaromkar313@gmail.com>
2025-08-16 13:44:41 +0530

    Chenge all files

--- data/cart.js
+++ data/cart.js
@@ -4,19 +4,14 @@ if(!cart){
   cart = [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity : 2,
-    deliveryOptionId : '1'
+    deliveryOptionId : 1
   },
   {
     productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity : 1,
-    deliveryOptionId : '2'
-  },
-  {
-    productId : '54e0eccd-8f36-462b-b68a-8182611d9add',
-    quantity : 3,
-    deliveryOptionId : '3'
-  }]
-};
+    deliveryOptionId : 2
+  }
+]};
 
 function saveToStorage(){
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -43,7 +38,7 @@ export function addToCart(productId){
       cart.push({
           productId : productId,
           quantity : quantity,
-          deliveryOptionId : '1'
+          deliveryOptionId : 1
       });
   }
 
