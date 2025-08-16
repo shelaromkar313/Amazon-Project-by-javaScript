e5bc2ea281234f715b807b1dc04807a2abec51e1
omkar0605shelar <shelaromkar313@gmail.com>
2025-08-16 15:56:47 +0530

    Change all these

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -103,7 +103,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'Checked' : ''}
           class="delivery-option-input"
@@ -201,5 +203,13 @@ document.querySelectorAll('.js-save-link')
       quantityLabel.innerHTML = newQuantity;
 
       updateCartQuantity();
+    });
+  });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click',() => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
     });
   });
