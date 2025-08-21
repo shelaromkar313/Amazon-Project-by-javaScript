ddf07109c5e7ec7e4c0dec682afc31bbfea2df76
omkar0605shelar <shelaromkar313@gmail.com>
2025-08-21 15:19:26 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -126,7 +126,10 @@ export function loadProductsFetch() {
     });
     
     console.log('load products');
-  }); 
+  })
+  // .catch(() => {
+  //   console.log('Unexpected error. Please try again later.');
+  // });
 
   return promise;
 }
@@ -150,9 +153,15 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
-} 
+}
+
 
 /*
 export const products = [
